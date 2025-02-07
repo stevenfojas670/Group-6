f30a4c5031934f4c3e2c5bd669ae19d07ac8ac2b
jkasbohm <kasbohm@unlv.nevada.edu>
2025-02-06 19:14:02 -0800

    Moving this whole respository to a Linux box because I'm tired of seeing errors and trying to work around them.

--- tdd_lab/tests/test_counter.py
+++ tdd_lab/tests/test_counter.py
@@ -10,3 +10,21 @@ how to call the web service and assert what it should return.
 - The service must be able to update a counter by name.
 - The service must be able to read the counter
 """
+
+import pytest
+from src import app
+from src import status
+
+@pytest.fixture()
+def client():
+    """Fixture for Flask test client"""
+    return app.test_client()
+
+@pytest.mark.usefixtures("client")
+class TestCounterEndpoints:
+    """Test cases for Counter API"""
+
+    def test_create_counter(self, client):
+        """It should create a counter"""
+        result = client.post(r'\counter\foo')
+        assert result.status_code == status.HTTP_201_CREATED
