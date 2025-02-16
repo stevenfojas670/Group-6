71f288d0d3455977c7b0275484063e79e321d2b2
Alexander Baker <bakera21@unlv.nevada.edu>
2025-02-16 01:00:09 -0800

    Added missing assertion for Student 4 test case

--- ci_lab/tests/test_counter.py
+++ ci_lab/tests/test_counter.py
@@ -152,7 +152,7 @@ class TestCounterEndpoints:
 
     # ===========================
     # Test: Set a counter to a specific value
-    # Author: Student 4
+    # Author: Student 4: Alexander Baker
     # Modification: Ensure setting a counter to the same value does nothing.
     # ===========================
     def test_set_counter_to_value(self, client):
@@ -164,6 +164,7 @@ class TestCounterEndpoints:
         assert response.get_json() == {"test1": 5}
 
         # TODO: Add an assertion to check setting to the same value does not change it again
+        assert response.get_json() == client.put('/counters/test1/set/5').get_json()
 
     # ===========================
     # Test: Prevent negative counter values
