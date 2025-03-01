60e0a59331b673f5d0fa5fe4d84dc523289d8fe8
Tweetiebird987 <sarelerasmus856@gmail.com>
2025-02-28 18:06:40 -0800

    Using ChatGPT, Improved Algorithm

--- gen_ai/task2.1/SarelErasmus_refactor_code.py
+++ gen_ai/task2.1/SarelErasmus_refactor_code.py
@@ -1,7 +1,9 @@
-def fib(n):
-  if n == 0 or n == 1:
-    return n
-  else:
-    return fib(n-1) + fib(n-2)
+def fib(n, memo={}):
+    if n in memo:
+        return memo[n]
+    if n == 0 or n == 1:
+        return n
+    memo[n] = fib(n-1, memo) + fib(n-2, memo)
+    return memo[n]
   
 print(fib(10))
